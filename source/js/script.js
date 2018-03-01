5471499162b2c682741076bf94a82118fc2fe107
mglushakova <glushakova7@mail.ru>
2018-03-01 16:05:41 +0500

    Добавляет первоначальную проверку содержимого полей ввода для позиционирования подписей

--- source/js/script.js
+++ source/js/script.js
@@ -43,6 +43,10 @@ for ( let i = 0; i < selectItems.length; i++ ) {
 let textInputs = document.querySelectorAll(".form__input[type='text'], .form__input[type='email']");
 
 for ( let i = 0; i < textInputs.length; i++ ) {
+  if ( event.target.value ) {
+    label.classList.add("form__label--small");
+  }
+  
   textInputs[i].addEventListener("blur", (event) => {
     let label = event.target.parentElement.querySelector(".form__label");
 
