448cb8d936a3759eb5ecdb24ccc29f158aff512e
mglushakova <glushakova7@mail.ru>
2018-03-01 14:42:23 +0500

    Добавляет валидацию формы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,35 @@
 "use strict";
 
+//Form open
+let popupButton = document.querySelector(".popup");
+let registerBlock = document.querySelector(".register");
+let firstItem = registerBlock.querySelector(".form__item")
+let firstInput = firstItem.querySelector(".form__input") ? firstItem.querySelector(".form__input") : firstItem.querySelector(".form__select");
+
+popupButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  registerBlock.classList.add("register--active");
+  firstInput.focus();
+});
+
+//Form close
+let closeButton = document.querySelector(".register__close");
+
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  registerBlock.classList.remove("register--active");
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.keyCode === 27) {
+    if (registerBlock.classList.contains("register--active")) {
+      registerBlock.classList.remove("register--active");
+    }
+  }
+});
+
 //Select focus on item click
 let selectItems = document.querySelectorAll(".form__item--select");
 
@@ -20,4 +50,64 @@ for ( let i = 0; i < textInputs.length; i++ ) {
       label.classList.add("form__label--small");
     }
   });
+}
+
+//Form validation
+let form = registerBlock.querySelector(".form");
+let formInputs = form.querySelectorAll(".form__input");
+let formSelects = form.querySelectorAll(".form__select");
+
+form.addEventListener("submit", (event) => {
+  for ( let i = 0; i < formInputs.length; i++ ) {
+    let inputType = formInputs[i].getAttribute("type");
+    let formItem = formInputs[i].closest(".form__item");
+    deletePrompt(formInputs[i], formItem);
+
+    if ( formInputs[i].hasAttribute("required") &&  !formInputs[i].value ) {
+      event.preventDefault();
+
+      formItem.classList.add("form__item--invalid");
+      createPrompt(formItem, "This is required field");
+    }
+
+    if ( formInputs[i].value && inputType == "email" &&  !validateEmail(formInputs[i].value) ) {
+      event.preventDefault();
+
+      formItem.classList.add("form__item--invalid");
+      createPrompt(formItem, "Invalid format");
+    }
+  }
+
+  for ( let i = 0; i < formSelects.length; i++ ) {
+    let formItem = formSelects[i].closest(".form__item");
+    deletePrompt(formSelects[i], formItem);
+
+    if ( formSelects[i].hasAttribute("required") && !formSelects[i].value ) {
+      event.preventDefault();
+
+      formItem.classList.add("form__item--invalid");
+      createPrompt(formItem, "This is required field");
+    }
+  }
+});
+
+function createPrompt(parentObject, message) {
+  let prompt = document.createElement("div");
+  prompt.classList.add("form__prompt");
+  prompt.innerText = message;
+
+  parentObject.appendChild(prompt);
+}
+
+function deletePrompt(input, item) {
+  let prompt = item.querySelector(".form__prompt");
+  if ( prompt ) {
+    item.classList.remove("form__item--invalid");
+    item.removeChild(prompt);
+  }
+}
+
+function validateEmail(email) {
+  var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return reg.test(email);
 }
